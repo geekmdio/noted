753c1dc11705b09fe99327c0e73b2745cfcb93de
Travis Nesbit <travis@geekmd.io>
2018-08-04 01:57:57 -0700

    Testing note builder

---
+++ test/create_test/notebuilder_test.go
@@ -0,0 +1,56 @@
+package create_test
+
+import (
+	"testing"
+	"github.com/geekmdio/noted/src/create"
+	"github.com/google/uuid"
+	"github.com/golang/protobuf/ptypes/timestamp"
+	"time"
+	"fmt"
+)
+
+func TestNoteBuilderInitSetsNewGuid(t *testing.T) {
+	b := create.NoteBuilder{}
+	note := b.Init().Build()
+
+	_, err := uuid.Parse(note.NoteGuid)
+
+	if err != nil {
+		t.Errorf("Could not parse note guid and it was therefore likely not set properly: %v", err)
+	}
+}
+
+func TestNoteBuilderInitCreatesNewNoteFragmentSliceWithZeroLength(t *testing.T) {
+	b := create.NoteBuilder{}
+	note := b.Init().Build()
+
+	if note.Fragments == nil || len(note.Fragments) != 0 {
+		t.Errorf("The Note Fragments were not properly initialized.")
+	}
+}
+
+func TestNoteBuilderSetIdSetsProperFieldAndReturnsProperValue(t *testing.T) {
+	b := create.NoteBuilder{}
+	var id int32 = 43
+	note := b.Init().SetId(id).Build()
+
+	if note.GetId() != id {
+		t.Errorf("Expecting %v and recieved %v", id, note.GetId())
+	}
+}
+
+func TestSetDateCreatedSetsTimeStampAndReturnsProperValue(t *testing.T) {
+	b := create.NoteBuilder{}
+	now := timestamp.Timestamp{Seconds:time.Now().Unix()}
+	note := b.Init().SetDateCreated(&now).Build()
+
+	fmt.Println(now.Seconds)
+
+	// Test made in Aug 2018, therefore the value for seconds since UNIX epoch
+	// must be greater than January 2018, which is about 40 years.
+	// 40 yr after UNIX epoch * 365 day/yr * 24 hrs/day * 60 min/hr * 60 sec/min
+	var jan2018 int64 = 1261440000
+	if note.DateCreated.Seconds <= jan2018 {
+		t.Errorf("Time for date created is less than or equal to zero; it was not properly set.")
+	}
+}
