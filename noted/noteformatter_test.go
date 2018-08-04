ad458dc9485a205792d1cdc9a5ae0efe5bb44aa3
Travis Nesbit <travis@geekmd.io>
2018-08-04 15:14:19 -0700

    [skip ci] Skeletoned the not formatter test

--- noted/noteformatter_test.go
+++ noted/noteformatter_test.go
@@ -3,40 +3,43 @@ package noted
 import (
 	"testing"
 	"github.com/geekmdio/noted/ehrproto"
+	"fmt"
 )
 
 func TestNoteFormatter(t *testing.T) {
 	nb := NoteBuilder{}
-	nfb := NoteFragmentBuilder{}
-
 	note := nb.Init().Build()
-	subjFrag := nfb.SetTopic(ehrpb.FragmentTopic_SUBJECTIVE).Build()
-	mhx := nfb.SetTopic(ehrpb.FragmentTopic_MEDICAL_HISTORY).Build()
-	all := nfb.SetTopic(ehrpb.FragmentTopic_ALLERGIES).Build()
-	meds := nfb.SetTopic(ehrpb.FragmentTopic_MEDICATIONS).Build()
-	fam := nfb.SetTopic(ehrpb.FragmentTopic_FAMILY_HISTORY).Build()
-	soc := nfb.SetTopic(ehrpb.FragmentTopic_SOCIAL_HISTORY).Build()
-	vit := nfb.SetTopic(ehrpb.FragmentTopic_VITALS).Build()
-	pe := nfb.SetTopic(ehrpb.FragmentTopic_PHYSICAL_EXAM).Build()
-	asthma := nfb.SetTopic(ehrpb.FragmentTopic_MEDICAL_PROBLEM).SetIssue(ehrpb.MedicalIssue_ASTHMA).Build()
-	cad := nfb.SetTopic(ehrpb.FragmentTopic_MEDICAL_PROBLEM).SetIssue(ehrpb.MedicalIssue_ISCHEMIC_CORONARY_ARTERY_DISEASE).Build()
+
+	subjFragBuilder := NoteFragmentBuilder{}
+	subjFragBuilder.InitFromNote(note)
+	subjFrag := subjFragBuilder.SetTopic(ehrpb.FragmentTopic_SUBJECTIVE).Build()
+
+	mhxFragBuilder := NoteFragmentBuilder{}
+	mhxFragBuilder.InitFromNote(note)
+	mhx := mhxFragBuilder.SetTopic(ehrpb.FragmentTopic_MEDICAL_HISTORY).Build()
+
+	allFragBuilder := NoteFragmentBuilder{}
+	allFragBuilder.InitFromNote(note)
+	all := allFragBuilder.SetTopic(ehrpb.FragmentTopic_ALLERGIES).Build()
+
+	medsFragBuilder := NoteFragmentBuilder{}
+	medsFragBuilder.InitFromNote(note)
+	meds := medsFragBuilder.SetTopic(ehrpb.FragmentTopic_MEDICATIONS).Build()
+
+	famFragBuilder := NoteFragmentBuilder{}
+	famFragBuilder.InitFromNote(note)
+	fam := famFragBuilder.SetTopic(ehrpb.FragmentTopic_FAMILY_HISTORY).Build()
+
+
+	soc := subjFragBuilder.SetTopic(ehrpb.FragmentTopic_SOCIAL_HISTORY).Build()
+	vit := subjFragBuilder.SetTopic(ehrpb.FragmentTopic_VITALS).Build()
+	pe := subjFragBuilder.SetTopic(ehrpb.FragmentTopic_PHYSICAL_EXAM).Build()
+	asthma := subjFragBuilder.SetTopic(ehrpb.FragmentTopic_MEDICAL_PROBLEM).SetIssue(ehrpb.MedicalIssue_ASTHMA).Build()
+	cad := subjFragBuilder.SetTopic(ehrpb.FragmentTopic_MEDICAL_PROBLEM).SetIssue(ehrpb.MedicalIssue_ISCHEMIC_CORONARY_ARTERY_DISEASE).Build()
 
 	note.Fragments = append(note.Fragments, vit, all, meds, subjFrag, cad, asthma, mhx, fam, soc, pe)
 
 	NoteFormatter(note)
-}
 
-//type FragmentTopic int32
-//
-//const (
-//	FragmentTopic_NO_TOPIC        FragmentTopic = 0
-//	FragmentTopic_SUBJECTIVE      FragmentTopic = 1
-//	FragmentTopic_MEDICAL_HISTORY FragmentTopic = 2
-//	FragmentTopic_ALLERGIES       FragmentTopic = 3
-//	FragmentTopic_MEDICATIONS     FragmentTopic = 4
-//	FragmentTopic_FAMILY_HISTORY  FragmentTopic = 5
-//	FragmentTopic_SOCIAL_HISTORY  FragmentTopic = 6
-//	FragmentTopic_VITALS          FragmentTopic = 7
-//	FragmentTopic_PHYSICAL_EXAM   FragmentTopic = 8
-//	FragmentTopic_MEDICAL_PROBLEM FragmentTopic = 9
-//)
+	fmt.Println(note)
+}
